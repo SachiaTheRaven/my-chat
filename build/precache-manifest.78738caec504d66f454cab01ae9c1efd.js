5b1b2d0f7eecebd714373a8bcae0d392a9273862
Sachia <szucspalmi@gmail.com>
2020-05-21 14:19:01 +0200

    build added

---
+++ build/precache-manifest.78738caec504d66f454cab01ae9c1efd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa428987e35431fbe3b47238ba1d6090",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5920160ae82ac55187f6",
+    "url": "./static/css/main.96585dd0.chunk.css"
+  },
+  {
+    "revision": "07c155597dc075058377",
+    "url": "./static/js/2.821ad3b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.821ad3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5920160ae82ac55187f6",
+    "url": "./static/js/main.50e8fae5.chunk.js"
+  },
+  {
+    "revision": "763f551d24bf50b1938c",
+    "url": "./static/js/runtime-main.c7b6dbd4.js"
+  }
+]);
